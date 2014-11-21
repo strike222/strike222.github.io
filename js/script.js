3fb6f2198c19f6424dd397d0ae99e24926c41791
mytory <mytory@gmail.com>
2014-11-21 13:34:27 +0900

    .

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-	$('h1, h2, h3').wordBreakKeepAll({OffForIE: true});
+  $('h1, h2, h3').wordBreakKeepAll({OffForIE: true});
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-56980141-1', 'auto');
+  ga('send', 'pageview');
 });
